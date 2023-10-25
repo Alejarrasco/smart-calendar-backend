c0ec624661a68c44e70df8c81860fe4045a5f0c2
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2023-10-24 21:12:00 -0400

    init commit

---
+++ src/main/java/bo/ucb/edu/smartcalendar/SmartcalendarApplication.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.smartcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/ucb/edu/smartcalendar/SmartcalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.smartcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
