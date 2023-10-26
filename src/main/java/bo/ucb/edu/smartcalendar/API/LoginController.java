dd19af8df341e34fc9661bae9a4f051864e0f930
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2023-10-26 15:42:59 -0400

    ahora se comunican mejor con el front

--- src/main/java/bo/ucb/edu/smartcalendar/API/LoginController.java
+++ src/main/java/bo/ucb/edu/smartcalendar/API/LoginController.java
@@ -1,16 +1,23 @@
 package bo.ucb.edu.smartcalendar.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import bo.ucb.edu.smartcalendar.dto.TestiDto;
+
 @RestController
 @RequestMapping(path = "/api/v1")
 public class LoginController {
-    @GetMapping (path = "/protected")
-    public void login(@RequestHeader("Authorization") String token){
+
+    @PostMapping (path = "/protected")
+    public TestiDto login(@RequestHeader("Authorization") String token, @RequestHeader("User-Agent") String userAgent, @RequestBody TestiDto body){
 
         System.out.println("Hello World "+token);
+        System.out.println("you are "+body.getName());
+        return new TestiDto(userAgent);
     }
 }
---
+++ src/main/java/bo/ucb/edu/smartcalendar/dto/TestiDto.java
@@ -0,0 +1,16 @@
+package bo.ucb.edu.smartcalendar.dto;
+
+public class TestiDto {
+    String name;
+
+    public TestiDto() {
+    }
+
+    public TestiDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
